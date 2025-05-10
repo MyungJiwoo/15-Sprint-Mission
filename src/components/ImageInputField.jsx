24ba981cfb9322d4d1e39550690a82aac4888697
MyungJiwoo <1206jiwoo@gmail.com>
2025-05-10 21:59:48 +0900

    refactor: 상품 이미지 input을 BaseImageInput, ImageInputField, ItemImageInputField로 분리

---
+++ src/components/ImageInputField.jsx
@@ -0,0 +1,119 @@
+import { memo } from "react";
+import styled from "@emotion/styled";
+import BaseImageInput from "@components/BaseImageInput";
+import PlusIcon from "@assets/icons/plus";
+import CloseIcon from "@assets/icons/close";
+
+const ImageInputField = ({
+  id,
+  label,
+  imageUrl,
+  onChange,
+  onDelete,
+  ...props
+}) => {
+  return (
+    <InputSection>
+      <Label>{label}</Label>
+      <ImageInputContainer>
+        <ImageLabel htmlFor={id}>
+          <PlusIcon />
+          <p>이미지 등록</p>
+          <BaseImageInput id={id} onChange={onChange} {...props} />
+        </ImageLabel>
+
+        {imageUrl && (
+          <PreviewImageContainer>
+            <PreviewImage src={imageUrl} alt="미리보기" />
+            <CloseIconBtn onClick={onDelete}>
+              <CloseIcon />
+            </CloseIconBtn>
+          </PreviewImageContainer>
+        )}
+      </ImageInputContainer>
+    </InputSection>
+  );
+};
+
+export default memo(ImageInputField);
+
+const InputSection = styled.div`
+  margin: 2rem 0;
+`;
+
+const Label = styled.label`
+  display: inline-block;
+  margin-bottom: 1rem;
+  font-weight: bold;
+  line-height: 3.2rem;
+  color: var(--gray900);
+  font-size: 1.8rem;
+`;
+
+const ImageLabel = styled.label`
+  width: 20rem;
+  height: 20rem;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  border-radius: 1.2rem;
+  background-color: var(--gray100);
+  cursor: pointer;
+
+  p {
+    font-size: 1.4rem;
+    color: var(--gray300);
+  }
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const ImageInputContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+
+const PreviewImageContainer = styled.div`
+  width: 20rem;
+  height: 20rem;
+  padding: 1rem;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  background-color: var(--white);
+  border: 1px solid var(--gray100);
+  border-radius: 1.2rem;
+`;
+
+const PreviewImage = styled.img`
+  width: 100%;
+  height: 100%;
+  aspect-ratio: 1/1;
+  object-fit: cover;
+  border-radius: 0.5rem;
+`;
+
+const CloseIconBtn = styled.div`
+  width: 2rem;
+  height: 2rem;
+  position: absolute;
+  top: 1.5rem;
+  right: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 100%;
+  background-color: var(--gray300);
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
