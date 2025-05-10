183b67c3605ebb1dcc7ed7de259c2ed2f513d9cf
MyungJiwoo <1206jiwoo@gmail.com>
2025-05-10 22:09:08 +0900

    fix: BaseForm에 누락된 props 추가

--- src/components/BaseForm.jsx
+++ src/components/BaseForm.jsx
@@ -1,5 +1,9 @@
-const BaseForm = ({ children, onSubmit }) => {
-  return <form onSubmit={onSubmit}>{children}</form>;
+const BaseForm = ({ children, onSubmit, ...props }) => {
+  return (
+    <form onSubmit={onSubmit} {...props}>
+      {children}
+    </form>
+  );
 };
 
 export default BaseForm;
